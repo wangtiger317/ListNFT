fea2a1f858df448895e940f645e1d7b786174ceb
wangtiger317 <hkc317@outlook.com>
2023-03-07 03:55:08 +0800

    updated site url

--- src/component/Card.js
+++ src/component/Card.js
@@ -11,8 +11,40 @@ export default function Card() {
         setListItem(item)
     }
     const getListNFt = async (address) => {
-        const { data } = await axios.get(`https://warehouse.czlabs.io/api1/web3/portfolio/${address}`, {})
-        setListData(data.data.collections)
+        let body = JSON.stringify({
+            "filter": {
+                "traits": {},
+                "traitsRange": {},
+                "address": address,
+                "rankRange": {},
+                "price": {}
+            },
+            "page": 1,
+            countPerPage: 25
+        })
+        // const { data } = await axios.get(`https://warehouse.czlabs.io/api1/web3/portfolio/${address}`, {})
+        const data = await fetch("https://bfgjytnlq1.execute-api.us-east-2.amazonaws.com/main/nftfilteredlistingsv2", {
+            "headers": {
+                "accept": "*/*",
+                "accept-language": "en-US,en;q=0.9",
+                "cache-control": "no-cache",
+                "content-type": "application/json",
+                "pragma": "no-cache",
+                "sec-ch-ua": "\"Chromium\";v=\"108\", \"Opera GX\";v=\"94\", \"Not)A;Brand\";v=\"99\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Windows\"",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "none"
+            },
+            "referrerPolicy": "strict-origin-when-cross-origin",
+            "body": body,
+            "method": "POST",
+            "mode": "cors",
+        })
+            .then(res => { return res.json() })
+        console.log(data)
+        setListData(data.data)
     }
     useEffect(() => {
         getListNFt(address)
@@ -38,14 +70,14 @@ export default function Card() {
                                 <div
                                     onClick={() => handleShowModal(item)}
                                     className='rounded w-full'
-                                    style={{ backgroundImage: `url(${item.image})`, backgroundSize: 'cover', height: '142px', }}
+                                    style={{ backgroundImage: `url(${item.imageUrl})`, backgroundSize: 'cover', height: '142px', }}
                                 />
                                 <span className='text-sm text-white truncate'>{item.name}</span>
                             </div>
                             <div className='bg-secondary flex flex-col gap-2 p-3 border-2 border-solid border-gray-300 rounded-b-lg'>
                                 <div className='text-xs text-gray-400'>Owner:</div>
                                 <div className='text-xs text-white truncate'>
-                                    {item.contract_address}
+                                    {item.address}
                                 </div>
                             </div>
 
